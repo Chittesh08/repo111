9e51c3df45b18de7059b85425332cfbbe269cdab
Chittesh08 <chittesh232005@gmail.com>
2025-01-25 13:09:27 +0530

    first commit

---
+++ cube.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class cube
+{
+public static void main(String[] args)
+{
+int i;
+
+Scanner scan =  new Scanner(System.in);
+System.out.print("Enter the Number for Cube:");
+int x = scan.nextInt();
+i=x*x*x;
+System.out.print("Cube of the Number is : " +i);
+}
+}
+
+
+
