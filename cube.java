ccb149ced527ce094f5395136437a070ffa5e7cc
chittesh8 <chittesh003@gmail.com>
2025-01-25 13:20:17 +0530

    Update cube.java
    done

--- cube.java
+++ cube.java
@@ -6,7 +6,7 @@ public static void main(String[] args)
 int i;
 
 Scanner scan =  new Scanner(System.in);
-System.out.print("Enter the Number for Cube:");
+System.out.print("Enter the  for Cube:");
 int x = scan.nextInt();
 i=x*x*x;
 System.out.print("Cube of the Number is : " +i);
